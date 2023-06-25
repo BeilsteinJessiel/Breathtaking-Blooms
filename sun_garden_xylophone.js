0abb80e990edcca46e426303dd91f8e72c666c87
BeilsteinJessiel <137533862+BeilsteinJessiel@users.noreply.github.com>
2023-06-26 02:59:02 +0700

    Refactor data structures

---
+++ sun_garden_xylophone.js
@@ -0,0 +1,469 @@
+// Florist JS File
+
+// 1. Global Variables
+var shopLocation;
+var deliveryArea;
+var storeHours;
+var floralArrangements;
+
+// 2. create shop location function
+function createShopLocation(location){
+	shopLocation = location;
+	console.log("The shop is located at: " + shopLocation);
+}
+
+// 3. create delivery area function
+function createDeliveryArea(area){
+	deliveryArea = area;
+	console.log("The delivery area is: " + deliveryArea);
+}
+
+// 4. create store hours function
+function createStoreHours(hours){
+	storeHours = hours;
+	console.log("The store hours are: " + storeHours);
+}
+
+// 5. create unique and personalized floral arrangements function
+function createFloralArrangements(arrangements){
+	floralArrangements = arrangements;
+	console.log("Unique and personalized floral arrangements are available: " + floralArrangements);
+}
+
+// 6. create orders function
+function createOrders(order){
+	console.log("A new order has been placed for: " + order);
+}
+
+// 7. create delivery function
+function createDelivery(recipient, address){
+	console.log("Delivering an order to " + recipient + " at " + address);
+}
+
+// 8. create payment process function
+function paymentProcess(paymentMethod){
+	console.log("Payment is being processed by: " + paymentMethod);
+}
+
+// 9. create customer feedback function
+function customerFeedback(feedback){
+	console.log("Customer feedback: " + feedback);
+}
+
+// 10. create store rating function
+function storeRating(rating){
+	console.log("The store rating is: " + rating);
+}
+
+// 11. create refresh flowers availability
+function refreshFlowersAvailability(){
+	console.log("Refreshing the availability of flowers...");
+}
+
+// 12. create display shop information on website function
+function displayShopInformation(){
+	console.log("Displaying shop information on the website.");
+}
+
+// 13. create add new employees function
+function addNewEmployees(employees){
+	console.log("Adding new employees to the shop: " + employees);
+}
+
+// 14. create update customer information function
+function updateCustomerInfo(){
+	console.log("Updating customer information...");
+}
+
+// 15. create update store inventory function 
+function updateStoreInventory(){
+	console.log("Updating the store inventory...");
+}
+
+// 16. create floral design ordering function 
+function floralDesignOrder(design){
+	console.log("Accepted floral design order: " + design);
+}
+
+// 17. create customer service function 
+function customerService(query){
+	console.log("Answering customer query: " + query);
+}
+
+// 18. create email notifications function
+function emailNotifications(notification){
+	console.log("Sending email notification: " + notification);
+}
+
+// 19. create new floral arrangement ideas function
+function newFloralArrangementIdeas(ideas){
+	console.log("Creating new floral arrangement ideas: " + ideas);
+}
+
+// 20. create customer follow up function
+function customerFollowUp(followUp){
+	console.log("Following up with customer: " + followUp);
+}
+
+// 21. create create floral design sketches function
+function createFloralDesignSketches(sketches){
+	console.log("Creating floral design sketches: " + sketches);
+}
+
+// 22. create order supplies function 
+function orderSupplies(supplies){
+	console.log("Ordering supplies for the shop: " + supplies);
+}
+
+// 23. create update website function
+function updateWebsite(){
+	console.log("Updating the website...");
+}
+
+// 24. create floral arrangements photos for website 
+function floralArrangementsPhotos(){
+	console.log("Adding photos of floral arrangements to the website...");
+}
+
+// 25. create wholesale pricing 
+function wholesalePricing(pricing){
+	console.log("Creating wholesale pricing structure: " + pricing);
+}
+
+// 26. create check for new flowers availability 
+function checkForNewFlowersAvailability(){
+	console.log("Checking for new flowers available in the shop...");
+}
+
+// 27. create customer referrals 
+function customerReferrals(referrals){
+	console.log("Received customer referrals: " + referrals);
+}
+
+// 28. create contact flower wholesalers 
+function contactFlowerWholesalers(wholesalers){
+	console.log("Contacting flower wholesalers: " + wholesalers);
+}
+
+// 29. create flower care instructions 
+function flowerCareInstructions(instructions){
+	console.log("Creating flower care instructions: " + instructions);
+}
+
+// 30. create flower arrangement tutorials 
+function flowerArrangementTutorials(tutorials){
+	console.log("Creating flower arrangement tutorials: " + tutorials);
+}
+
+// 31. create flower season updates 
+function flowerSeasonUpdates(){
+	console.log("Updating flower season information...");
+}
+
+// 32. create special offer promotion 
+function specialOfferPromotion(promotion){
+	console.log("Creating special offer promotion: " + promotion);
+}
+
+// 33. create customer loyalty program 
+function customerLoyaltyProgram(program){
+	console.log("Creating a customer loyalty program: " + program);
+}
+
+// 34. create shop events 
+function shopEvents(events){
+	console.log("Organizing shop events: " + events);
+}
+
+// 35. create shop newsletter 
+function shopNewsletter(newsletter){
+	console.log("Creating shop newsletter: " + newsletter);
+}
+
+// 36. create flower subscription service 
+function flowerSubscriptionService(service){
+	console.log("Creating a flower subscription service: " + service);
+}
+
+// 37. create unique floral designs 
+function uniqueFloralDesigns(designs){
+	console.log("Creating unique floral designs: " + designs);
+}
+
+// 38. create prepare bouquets 
+function prepareBouquets(bouquets){
+	console.log("Preparing bouquets: " + bouquets);
+}
+
+// 39. create floral arrangement schemes
+function floralArrangementSchemes(schemes){
+	console.log("Creating floral arrangement schemes: " + schemes);
+}
+
+// 40. create flower arranging techniques 
+function flowerArrangingTechniques(techniques){
+	console.log("Creating flower arranging techniques: " + techniques);
+}
+
+// 41. create attract customers function
+function attractCustomers(customers){
+	console.log("Attracting customers: " + customers);
+}
+
+// 42. create review upcoming orders 
+function reviewUpcomingOrders(){
+	console.log("Reviewing upcoming orders...");
+}
+
+// 43. create social media campaigns 
+function socialMediaCampaigns(campaigns){
+	console.log("Creating social media campaigns: " + campaigns);
+}
+
+// 44. create customer interaction 
+function customerInteraction(interaction){
+	console.log("Interacting with customers: " + interaction);
+}
+
+// 45. create customer feedback surveys 
+function customerFeedbackSurveys(surveys){
+	console.log("Creating customer feedback surveys: " + surveys);
+}
+
+// 46. create business cards 
+function createBusinessCards(cards){
+	console.log("Creating business cards: " + cards);
+}
+
+// 47. create local business advertising 
+function createLocalBusinessAdvertising(advertising){
+	console.log("Creating local business advertising: " + advertising);
+}
+
+// 48. create unique product ideas 
+function createUniqueProductIdeas(ideas){
+	console.log("Creating unique product ideas: " + ideas);
+}
+
+// 49. create new flower arrangements 
+function createNewFlowerArrangements(arrangements){
+	console.log("Creating new flower arrangements: " + arrangements);
+}
+
+// 50. create custom flower arrangements 
+function createCustomFlowerArrangements(arrangements){
+	console.log("Creating custom flower arrangements: " + arrangements);
+}
+
+// 51. create flower care tips 
+function createFlowerCareTips(tips){
+	console.log("Creating flower care tips: " + tips);
+}
+
+// 52. create shop signage 
+function createShopSignage(signage){
+	console.log("Creating shop signage: " + signage);
+}
+
+// 53. create shop promotions 
+function createShopPromotions(promotions){
+	console.log("Creating shop promotions: " + promotions);
+}
+
+// 54. create package designs 
+function createPackageDesigns(designs){
+	console.log("Creating package designs: " + designs);
+}
+
+// 55. create shop window display 
+function createShopWindowDisplay(display){
+	console.log("Creating shop window display: " + display);
+}
+
+// 56. create shop reviews 
+function createShopReviews(reviews){
+	console.log("Creating shop reviews: " + reviews);
+}
+
+// 57. create customer loyalty reward program 
+function createCustomerLoyaltyRewardProgram(program){
+	console.log("Creating a customer loyalty reward program: " + program);
+}
+
+// 58. create flower arrangements inspirations 
+function createFlowerArrangementsInspirations(inspirations){
+	console.log("Creating flower arrangements inspirations: " + inspirations);
+}
+
+// 59. create flower arranging classes 
+function createFlowerArrangingClasses(classes){
+	console.log("Creating flower arranging classes: " + classes);
+}
+
+// 60. create customer appreciation 
+function createCustomerAppreciation(appreciation){
+	console.log("Creating customer appreciation: " + appreciation);
+}
+
+// 61. create flower shop blog 
+function createFlowerShopBlog(blog){
+	console.log("Creating flower shop blog: " + blog);
+}
+
+// 62. create customer satisfaction survey 
+function createCustomerSatisfactionSurvey(survey){
+	console.log("Creating a customer satisfaction survey: " + survey);
+}
+
+// 63. create flower subscription service 
+function createFlowerSubscriptionService(service){
+	console.log("Creating a flower subscription service: " + service);
+}
+
+// 64. create shop tour 
+function createShopTour(tour){
+	console.log("Creating a shop tour: " + tour);
+}
+
+// 65. create online shop 
+function createOnlineShop(shop){
+	console.log("Creating an online shop: " + shop);
+}
+
+// 66. create workshops 
+function createWorkshops(workshops){
+	console.log("Creating workshops: " + workshops);
+}
+
+// 67. create flower care guide 
+function createFlowerCareGuide(guide){
+	console.log("Creating a flower care guide: " + guide);
+}
+
+// 68. create special event arrangements 
+function createSpecialEventArrangements(arrangements){
+	console.log("Creating special event arrangements: " + arrangements);
+}
+
+// 69. create website FAQ page 
+function createWebsiteFAQPage(page){
+	console.log("Creating a website FAQ page: " + page);
+}
+
+// 70. create payment options 
+function createPaymentOptions(options){
+	console.log("Creating payment options: " + options);
+}
+
+// 71. create marketing materials 
+function createMarketingMaterials(materials){
+	console.log("Creating marketing materials: " + materials);
+}
+
+// 72. create online catalog 
+function createOnlineCatalog(catalog){
+	console.log("Creating an online catalog: " + catalog);
+}
+
+// 73. create floral design services 
+function createFloralDesignServices(services){
+	console.log("Creating floral design services: " + services);
+}
+
+// 74. create photo galleries 
+function createPhotoGalleries(galleries){
+	console.log("Creating photo galleries: " + galleries);
+}
+
+// 75. create flower delivery services 
+function createFlowerDeliveryServices(services){
+	console.log("Creating flower delivery services: " + services);
+}
+
+// 76. create flower subscription options 
+function createFlowerSubscriptionOptions(options){
+	console.log("Creating flower subscription options: " + options);
+}
+
+// 77. create flower arrangement classes 
+function createFlowerArrangementClasses(classes){
+	console.log("Creating flower arrangement classes: " + classes);
+}
+
+// 78. create gift bouquets 
+function createGiftBouquets(bouquets){
+	console.log("Creating gift bouquets: " + bouquets);
+}
+
+// 79. create gift shop items 
+function createGiftShopItems(items){
+	console.log("Creating gift shop items: " + items);
+}
+
+// 80. create combine flower bouquets
+function createCombineFlowerBouquets(bouquets){
+	console.log("Creating combine flower bouquets: " + bouquets);
+}
+
+// 81. create promotional offers 
+function createPromotionalOffers(offers){
+	console.log("Creating promotional offers: " + offers);
+}
+
+// 82. create email campaigns 
+function createEmailCampaigns(campaigns){
+	console.log("Creating email campaigns: " + campaigns);
+}
+
+// 83. create flower delivery tracking 
+function createFlowerDeliveryTracking(tracking){
+	console.log("Creating flower delivery tracking: " + tracking);
+}
+
+// 84. create subscription services packages 
+function createSubscriptionServicesPackages(packages){
+	console.log("Creating subscription services packages: " + packages);
+}
+
+// 85. create flower arranging tools 
+function createFlowerArrangingTools(tools){
+	console.log("Creating flower arranging tools: " + tools);
+}
+
+// 86. create customer service support 
+function createCustomerServiceSupport(support){
+	console.log("Creating customer service support: " + support);
+}
+
+// 87. create referral program 
+function createReferralProgram(program){
+	console.log("Creating a referral program: " + program);
+}
+
+// 88. create customer satisfaction survey analysis 
+function createCustomerSatisfactionSurveyAnalysis(analysis){
+	console.log("Creating a customer satisfaction survey analysis: " + analysis);
+}
+
+// 89. create flower care tips videos 
+function createFlowerCareTipsVideos(videos){
+	console.log("Creating flower care tips videos: " + videos);
+}
+
+// 90. create flower bouquet designs 
+function createFlowerBouquetDesigns(designs){
+	console.log("Creating flower bouquet designs: " + designs);
+}
+
+// 91. create flower arrangement classes tutorials 
+function createFlowerArrangementClassesTutorials(tutorials){
+	console.log("Creating flower arrangement classes tutorials: " + tutorials);
+}
+
+// 92. create flower delivery options 
+function createFlowerDeliveryOptions(options){
+	console.log("Creating flower delivery options: " + options);
+}
+
+// 93. create unique product photos 
+function createUniqueProductPhotos
